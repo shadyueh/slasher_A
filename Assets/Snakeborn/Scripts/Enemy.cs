59841820e6c479a7e2403cef8395c891d54fccac
Unknown <yuri.costaoliveira@gmail.com>
2020-01-17 16:55:13 -0300

    Importação do modelo, texturas e animações da Onça

---
+++ Assets/Snakeborn/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float TotalHealth;
+    public float CurrentHealth;
+    public float AttackDamage;
+    public float MovementSpeed;
+
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetHit()
+    {
+        //anim.SetInteger("transition", 0);
+        //StartCoroutine(RecoveryFromHit());
+    }
+    IEnumerator RecoveryFromHit()
+    {
+        yield return new WaitForSeconds(0.5f);
+    }
+}
